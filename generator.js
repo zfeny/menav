66fdccad59611a73317dde162fb0a7b4cf709932
Zuoling Rong <rzlrong@outlook.com>
2025-02-01 17:12:23 +0800

    测试

--- generator.js
+++ generator.js
@@ -102,8 +102,9 @@ function generateHTML(config) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${config.site.title}</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/menav/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <base href="/menav/">
 </head>
 <body class="loading">
     <div class="layout">
